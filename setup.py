18351c5d59686fde64f4921c46c2b2c8b9cde9eb
jakubbaginski <44910915+jakubbaginski@users.noreply.github.com>
2021-09-12 22:36:29 +0200

    update of setup

--- setup.py
+++ setup.py
@@ -1,20 +1,18 @@
-import setuptools.config
 from setuptools import setup, find_packages
 
 setup(
     name="pythonbootcamp",
-    version='0.0.2',
+    version='0.0.3',
+    long_description='Python Pro Bootcamp 2021 from Udemy',
     packages=find_packages('./src/'),
-    package_dir={
-        'pythonbootcamp': 'src/pythonbootcamp/',
-        'pythonbootcamp.day032': 'src/pythonbootcamp/day032/'
-    },
+    package_dir={'': 'src'},
     package_data={'pythonbootcamp.day032': ['data/*.txt']},
 
     author='Jakub Bagiński',
     author_email='jakub.baginski@gmail.com',
     url='https://github.com/jakubbaginski/pythonbootcamp',
-    license=''
+    license='GPU',
+    license_files=['LICENSE']
 )
 
 print(f'Packages found: {find_packages("./src/")}')
