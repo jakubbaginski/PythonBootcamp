fe7c865042f14724e91a16418402a660fc00261a
jakubbaginski <44910915+jakubbaginski@users.noreply.github.com>
2021-09-13 00:46:45 +0200

    email-sender

--- src/pythonbootcamp/day032/__init__.py
+++ src/pythonbootcamp/day032/__init__.py
@@ -1,9 +1,14 @@
 __all__ = [
 ]
 
+import json
 import random
 import re
 import pkg_resources
+import smtplib
+import ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 
 class MotivationQuote:
@@ -20,6 +25,35 @@ class MotivationQuote:
         return self.data[random.randint(0, len(self.data)-1)]
 
 
+def email_sender():
+    with open('data/email_config.json', 'r') as config_file:
+        config_data = json.load(config_file)
+    print(config_data['host'], ":",int(config_data['port']))
+    with smtplib.SMTP_SSL(config_data['host'], int(config_data['port']),
+                          ssl.create_default_context()) as connection:
+        connection.login(user=config_data['email_from'], password=config_data['email_from_passwd'])
+
+        message = MIMEMultipart("alternative")
+        message["Subject"] = "Motivation Quote"
+        message["From"] = config_data['email_from']
+        message["To"] = config_data['email_to']
+
+        text = MotivationQuote().random_quote()
+
+        html = f"""
+            <html><body><h3>
+            <i>{text['text']}</i><br>
+            </h3>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;{text['author']}        
+            </body><html>
+            """
+
+        message.attach(MIMEText(html, "html"))
+        connection.sendmail(from_addr=config_data['email_from'], to_addrs=config_data['email_to'],
+                            msg=message.as_string())
+
+
 if __name__ == '__main__':
     print("Example output:")
     print(MotivationQuote().random_quote())
+    email_sender()
