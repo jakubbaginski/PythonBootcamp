7f5883e8b6c75eed7dccee3cc5b2ea6919cc9d89
jakubbaginski <44910915+jakubbaginski@users.noreply.github.com>
2021-09-14 14:24:53 +0200

    debugging code removed

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import pythonbootcamp.day032
-import os
 
-os.chdir('..')
-print(os.getcwd())
-print(pythonbootcamp.day032.MotivationQuote().random_quote())
+
+# print(pythonbootcamp.day032.MotivationQuote().random_quote())
 pythonbootcamp.day032.AutomatedBirthdayWisher().run(config_file_name='data/secret.json')
