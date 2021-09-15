bd2975ce3719978036e8e039d6952c440595532f
jakubbaginski <44910915+jakubbaginski@users.noreply.github.com>
2021-09-15 02:12:02 +0200

    day033 iss tracker almost ready, the code obtains an observer's position (based on address), iss position and sunrise/sunset times

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pythonbootcamp.day032
 import pythonbootcamp.day033
 import os
-os.chdir('..')
+os.chdir('../../../')
 
 # print(pythonbootcamp.day032.MotivationQuote().random_quote())
 # pythonbootcamp.day032.AutomatedBirthdayWisher().run(config_file_name='data/secret.json', sender_name='Kuba')
 
 
-pythonbootcamp.day033.KanyeApp().mainloop()
+# pythonbootcamp.day033.KanyeApp().mainloop()
+pythonbootcamp.day033.ISSTracker('Gdynia, Dionizosa').get_iss_position()
--- src/pythonbootcamp/day033/__init__.py
+++ src/pythonbootcamp/day033/__init__.py
@@ -2,6 +2,8 @@ import tkinter
 import tkinter.ttk
 import pkg_resources
 import requests
+import geocoder
+from datetime import datetime as dt
 
 
 class KanyeApp(tkinter.Tk):
@@ -31,3 +33,38 @@ class KanyeApp(tkinter.Tk):
         self.canvas.itemconfig(self.quote_text, text=response.json()['quote'])
 
 
+class ISSTracker:
+
+    def __init__(self, location: str):
+        # geolocation API
+        g = geocoder.osm(location)
+        self.lat = g.geojson['features'][0]['properties']['lat']
+        self.lng = g.geojson['features'][0]['properties']['lng']
+
+        self.sunset_times: dict = {}
+        self.iss_position: dict = {}
+        self.curr_datetime: dt = dt.fromisoformat(dt.utcnow().astimezone().isoformat(sep=' ', timespec='seconds'))
+
+    def get_sunset_times(self):
+        # sunrise/sunset API
+        response = requests.get('https://api.sunrise-sunset.org/json',
+                                {'lat': self.lat, 'lng': self.lng, 'formatted': 0})
+        response.raise_for_status()
+        self.sunset_times: dict = response.json()['results']
+
+        local_time_result = {key: dt.fromisoformat(self.sunset_times[key]).astimezone()
+                             for key in self.sunset_times if key != 'day_length'}
+        self.sunset_times.update(local_time_result)
+
+    def get_iss_position(self):
+        response = requests.get('http://api.open-notify.org/iss-now.json')
+        response.raise_for_status()
+        self.iss_position: dict = response.json()
+        print(self.iss_position)
+
+    def is_iss_overhead(self):
+        # has to be night
+        if self.sunset_times['sunset'] > self.curr_datetime or\
+                self.sunset_times['sunrise'] < self.curr_datetime:
+        # TODO: Compare my position with iss position
+            pass
